d054562041982eed06c7bba52698145ff9d95484
Nikita Zemlevskiy <naz7@duke.edu>
2017-04-23 15:13:25 -0400

    fix setup with duplicated code in move and state

--- Move.py
+++ Move.py
@@ -1,64 +1,7 @@
 import numpy as np
-from utils import *
+from State import State
 
-class Move:
-	'Move class to express CAMEO moves in terms of Resources and Infrastructure'
-	"""
-	Attributes:
-		resources: a dictionary expressing the change in resources caused by the move
-		infrastructure: a dictionary expressing the change in infrastructure caused by the move
-	"""
-	def __init__(self, resources, infrastructure):
-		self.resources = resources
-		self.infrastructure = infrastructure
-
-	"""
-	Reads in the values of each resource measure and adds it to the resource dictionary
-	"""
-	def parseMeasures(self, filename):
-		print ("WILL UPDATE ONCE I GET THE CSV FORMAT FROM THE SMRs")
-
-
-	"""
-	Used to get the measure of the resource or infrastructure from the respective dictionary
-	'r' = resource
-	'i' = infrastructure
-	"""
-	def getMeasure(self, measure, category = 'r'):
-		measure = formatMeasure(measure)
-		if category != 'r' and category != 'i':
-			raise ValueError("Please input the category as either 'r', resources, or 'i', infrastructure")
-		if category == "r" and measure not in self.resources:
-			raise ValueError("'%s' not in resources" %measure)
-		elif category == "i" and measure not in self.infrastructure:
-			raise ValueError("%s not in infrastructure" %measure)
-
-		else:
-			if (category == "r"):
-				return self.resources[measure]
-			elif (category == "i"):
-				return self.infrastructure[measure]
-
-	"""
-	Used to update the measure of the resource or infrastructure from the respective dictionary
-	'r' = resource
-	'i' = infrastructure
-	"""
-	def updateMeasure(self, measure, value, category = 'r'):
-		measure = formatMeasure(measure)
-		if category != 'r' and category != 'i':
-			raise ValueError("Please input the category as either 'r', resources, or 'i', infrastructure")
-		if category == "r" and measure not in self.resources:
-			raise ValueError("'%s' not in resources" %measure)
-		elif category == "i" and measure not in self.infrastructure:
-			raise ValueError("%s not in infrastructure" %measure)
-		else:
-			if (category == "r"):
-				self.resources[measure] = value
-			elif (category == "i"):
-				self.infrastructure[measure] = value
-
-
+class Move(State):
     '''
     given by monte carlo - gets the state resulting from this move being made by sampling a distribution
     '''
@@ -68,6 +11,7 @@ class Move:
 
     def compare(self, other):
         return np.sum([abs(self.pmesiiVars[i] - other.pmesiiVars[i]) for i in range(0, len(self.pmesiiVars))])
+
 
 '''
 move = Move({"PDRLEG": 4}, {"Oil Refineries": 5})
--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
-
-
 def formatMeasure(measure):
-	newMeasure = ""
-	for ch in measure:
-		if ch == " ":
-			newMeasure += "_"
-		else:
-			newMeasure += ch
-	return newMeasure.strip().lower()
+    newMeasure = ""
+    for ch in measure:
+        if ch == " ":
+            newMeasure += "_"
+        else:
+            newMeasure += ch
+    return newMeasure.strip().lower()
